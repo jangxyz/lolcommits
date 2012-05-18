b9d157c0ff55e049d090049bfac00148b7a96606
Kim, Jang-hwan <janghwan@gmail.com>
2012-05-18 16:41:56 +0900

    upload image to imgur and append image url to commit message
    * based on:
        https://github.com/youpy/lolcommits/commit/d62e59cdaf9530709eadf6f00c64564fb0d6e423
    * with --enable-imgur
    * and some refactorings
    
    lolcommits:
        !http://i.imgur.com/0g6HWl.jpg!

--- lib/lolcommits.rb
+++ lib/lolcommits.rb
@@ -48,11 +48,13 @@ module Lolcommits
     return loldir, commit_sha, commit_msg
   end
 
-  def upload(loldir, commit_sha)
+  def upload(filepath)
     client = Imgur2.new 'b5ca7b73c0885e3208e1907b3d587f97' # lolcommits API key
-    image = File.open File.join(loldir, "#{commit_sha}.jpg"), 'rb'
+
+    image = File.open File.join(filepath), 'rb'
     result = client.upload image
     image.close
+
     if result['error']
       STDERR.puts "Unable to upload lolcommit: #{result['error']['message']}"
     elsif result['upload']
@@ -62,15 +64,21 @@ module Lolcommits
       else
         puts "*** Image URL: #{url}"
       end
+      url
     end
   end
 
-  def capture(capture_delay=0, is_test=false, test_msg=nil, test_sha=nil)
+  def capture(msg_file, capture_delay=0, is_test=false, test_msg=nil, test_sha=nil)
     #
     # Read the git repo information from the current working directory
     #
     if not is_test
       loldir, commit_sha, commit_msg = parse_git
+
+      # overwrite commit_msg
+      open(msg_file) do |f|
+        commit_msg = f.readline.strip
+      end
     else
       commit_msg = test_msg
       commit_sha = test_sha
@@ -156,23 +164,38 @@ module Lolcommits
       self.pointsize = 48
       self.interline_spacing = -(48 / 5) if self.respond_to?(:interline_spacing)
       self.stroke_width = 2
-    end
+    end if commit_msg
 
     #
     # Squash the images and write the files
     #
-    #canvas.flatten_images.write("#{loldir}/#{commit_sha}.jpg")
-    #canvas.write(File.join loldir, "#{commit_sha}.jpg")
-    #FileUtils.rm(snapshot_loc)
-    file = Tempfile.new 'image'
-    canvas.write(file.path)
+    filename = Time.now.strftime("%Y%m%d_%H%M")
+    filepath = File.join loldir, "#{filename}.jpg"
+    canvas.write filepath
+    FileUtils.rm(snapshot_loc)
 
-    #if in test mode, open image for inspection
-    if is_test
-      Launchy.open(File.join loldir, "#{commit_sha}.jpg")
-    end
-    
-    
+    # if in test mode, open image for inspection
+    Launchy.open(filepath) if is_test
+
+    return filepath
+  end
+
+  def upload_and_append_to_commit_msg(img_file, msg_file)
+    url = upload img_file
+    puts url.inspect
+    open(msg_file, 'a') do |f|
+      f.write "\nlolcommits:\n    #{url}\n"
+    end if url
+  end
+
+  def capture_imgur(msg_file, capture_delay=0, is_test=false, test_msg=nil, test_sha=nil)
+    filepath = capture(msg_file, capture_delay, is_test, test_msg, test_sha)
+    upload_and_append_to_commit_msg(filepath, msg_file)
+    ## upload file and append to commit message
+    #url = upload filepath
+    #open(msg_file, 'a') do |f|
+    #  f.write "\nlolcommits:\n    #{url}\n"
+    #end if url
   end
   
 end
